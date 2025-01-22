eba92423d93393bb9e0790929273a60262ec830b
Sri Akash Kadali <kadali18@umd.edu>
2025-01-21 22:53:23 -0500

    Delete Easy directory

--- Easy/faultykeyboard.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def finalString(self, s: str) -> str:
-        words = s.split("i")
-        new_word = ""
-        for word in words:
-            new_word += word
-            new_word = new_word[::-1]
-        return new_word[::-1]
